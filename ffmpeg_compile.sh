0b008b9d8e12c44ed7bad65397dd9b6d477c4092
W∆L13R <76449145+ritwik-ritnov@users.noreply.github.com>
2021-10-21 13:14:51 +0530

    Update ffmpeg_compile.sh

--- ffmpeg_compile.sh
+++ ffmpeg_compile.sh
@@ -24,7 +24,7 @@ PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig"
 export PKG_CONFIG_PATH
 ./configure --prefix="$HOME/ffmpeg_build" \
   --extra-cflags="-I$HOME/ffmpeg_build/include" --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
-  --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfdk-aac --enable-libx264 \
+  --bindir="$HOME/bin" --extra-libs="-ldl" --enable-gpl --enable-libass --enable-libfdk-aac --enable-libx264 --enable-libx265\
   --enable-libmp3lame --enable-nonfree
 make
 make install
